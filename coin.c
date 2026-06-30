f5244214db8544a1c3d1295140f30eae13d1ba35
blackblack96sheep-png <blackblack96sheep@gmail.com>
2026-06-30 11:26:34 +0900

    Create coin.c

---
+++ coin.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+
+int main(){
+
+  int i;
+  int hcount,tcount;
+  hcount=tcount=0;
+  srand((unsigned int)time(NULL));
+
+  printf("Tossing a coin...\n");
+  for(i=1;i<=3;i++){
+    printf("Round %d:",i);
+    if(rand()%2==0){
+      printf("Heads\n");
+      hcount++;
+    }
+    else {
+      printf("Tails\n");
+      tcount;
+    }
+
+  }
+  
+
+
+  return 0;
+}
