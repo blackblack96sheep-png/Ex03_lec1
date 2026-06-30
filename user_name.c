d59e1f565ca1392cb730d40ef60ba6848eac04c2
blackblack96sheep-png <blackblack96sheep@gmail.com>
2026-06-30 12:05:05 +0900

    Create user_name.c
    user name added

---
+++ user_name.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+
+int main(){
+
+  int i;
+  int hcount,tcount;
+  string name[20];
+  hcount=tcount=0;
+  srand((unsigned int)time(NULL));
+  
+print("Who are you?\n->");  
+scanf("%s".&name);
+  print("Hello %s",name);
+
+  printf("Tossing a coin...\n");
+  for(i=1;i<=3;i++){
+    printf("Round %d:",i);
+    if(rand()%2==0){
+      printf("Heads\n");
+      hcount++;
+    }
+    else {
+      printf("Tails\n");
+      tcount;
+    }
+
+  }
+  
+
+
+  return 0;
+}
